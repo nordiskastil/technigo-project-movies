9ae8eb2fc680c21a3da721846be127ec44d3fac8
Nathalie <68371736+nordiskastil@users.noreply.github.com>
2025-07-13 10:08:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export const App = () => {
-  return <div>Find me in src/app.jsx!</div>
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PopularList from './components/PopularList'; // Importing the PopularList component
+import Detail from './components/Detail'; // Importing the Detail component
+import NotFound from './components/NotFound'; // Importing the NotFound component
+
+//Sets up your routes using React Router: /: Home route → shows popular movies, /movies/:id: Movie detail, *: Catch-all → shows 404 page
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<PopularList />} />
+      <Route path="/movies/:id" element={<Detail />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
